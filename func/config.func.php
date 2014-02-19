b8725fc40096998d9dab3062ce97a582dd5b8669
Carlos Cerrillos <carlos@cerrillostech.com>
2014-02-19 14:47:13 -0800

    Fixed Logging code

--- func/config.func.php
+++ func/config.func.php
@@ -1,28 +1,7 @@
 <?
-session_start();
 $db_host = "localhost";
 $db_user = "quiz";
 $db_pass = "quiz";
 $db_name = "iPoly";
 date_default_timezone_set("America/Los_Angeles");
-function makeChange($page, $guilty, $to){
-	global $db_host, $db_user, $db_pass, $db_name;
-	$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
-	if($query = $mysqli->prepare("SELECT * FROM `pages` WHERE page=?")){
-		$query->bind_param("s", $page);
-		$query->execute();
-		$query->bind_result($pages['pageid'], $pages['object'], $pages['access']);
-		if($stmt = $mysqli -> prepare("INSERT INTO `pagelogs` VALUE ('',?,?,?,?,?);")){
-			$stmt -> bind_param("sssss", $page, date(), $guilty, $pages['object'], $to);
-			$stmt -> execute();
-			$stmt -> close();
-		} else {
-			echo $mysqli->error;
-		}
-	} else {
-		echo $mysqli->error;
-	}
-	
-	$mysqli -> close();
-}
 ?>
--- func/edit.func.php
+++ func/edit.func.php
@@ -10,17 +10,33 @@ if($_POST['submit']=="Save" && isset($_SESSION['is_admin'])){
 	$tempObject = new PolyPage($pageType, $pageTitle);
 	$tempObject->setPageContent($pageContent);
 	$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
-	makeChange($page, $_SESSION['username'], $tempObject->serializeThis());
 	if($stmt = $mysqli -> prepare("UPDATE `pages` SET object=? WHERE pageid=?;")){
-				$stmt -> bind_param("ss",$tempObject->serializeThis(), $page);
+		$stmt -> bind_param("ss",$tempObject->serializeThis(), $page);
+		$stmt -> execute();
+		$stmt -> close();
+		if($query = $mysqli->prepare("SELECT * FROM `pages` WHERE pageid=?")){
+			$query->bind_param("s", $page);
+			$query->execute();
+			$query->bind_result($pages['pageid'], $pages['object'], $pages['access']);
+			$query->fetch();
+			$object = $pages['object'];
+			$query->close();
+			if($stmt = $mysqli -> prepare("INSERT INTO `pagelogs` VALUES ('',?,?,?,?,?);")){
+				$stmt -> bind_param("sssss", $page, time(), $_SESSION['username'], $object, $tempObject->serializeThis());
 				$stmt -> execute();
 				$stmt -> close();
 			} else {
 				echo $mysqli->error;
 			}
-			$mysqli -> close();
-			header('Location: ../?p='.$page);
+		} else {
+			echo $mysqli->error;
+		}
+	} else {
+		echo $mysqli->error;
+	}
+	$mysqli -> close();
+	//header('Location: ../?p='.$page);
 } else {
-	header('Location: ' . $_SERVER['HTTP_REFERER']);
+	//header('Location: ' . $_SERVER['HTTP_REFERER']);
 }
 ?>
