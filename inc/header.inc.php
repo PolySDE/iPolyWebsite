f4496fcfeab540d75015ffe3f5648e39c96bb191
Carlos Cerrillos <carlos@cerrillostech.com>
2014-02-18 16:17:54 -0800

    Made changes to inner CSS of the TinyMCE editor

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -27,6 +27,7 @@
 		<script>
                 tinymce.init({
 					selector:'textarea.tinymce',
+					content_css: "css/style.css, css/style-desktop.css",
 					plugins: [
                  "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
                  "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
