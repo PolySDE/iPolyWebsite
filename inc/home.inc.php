7d1baeee53a4ca6e2799d06cd85697a90270725c
Carlos Cerrillos <carlos@cerrillostech.com>
2014-03-10 16:39:49 -0700

    Misc changes to homepage

--- inc/home.inc.php
+++ inc/home.inc.php
@@ -34,7 +34,6 @@
                         ?>
 						<div class="row">
 							<div class="4u">
-
 								<!-- Feature -->
 									<section>
 										<a href="?p=about_mission" class="image image-center"><img src="images/mission_of_ipoly_home_page.jpg" alt=""/></a>
@@ -54,7 +53,7 @@
 										<header>
 											<h3>Project-Based Learning</h3>
 										</header>
-										<p>The foundation of iPoly is its Project-Based curriculum.</p>
+										<p>The foundation of iPoly is our Project-Based curriculum.</p>
 									</section>
 									
 							</div>
@@ -66,7 +65,7 @@
 										<header>
 											<h3>New Building</h3>
 										</header>
-										<p>Come explore our new building!</p>
+										<p>Take a virtual tour of our new campus!</p>
 									</section>
 
 							</div>
@@ -80,8 +79,6 @@
 					</section>
 			</div>
 		<!-- Main Wrapper -->
-        <?
-		/*
 			<div id="main-wrapper">
 				<!-- Main -->
 					<div id="main" class="container">
@@ -91,87 +88,47 @@
 									<!-- Post -->
 										<article class="is-post">
 											<header>
-												<h2 style="display:inline;"><a href="?p=pbl_workshops">Project Based Learning Workshops</a></h2>
+												<h2 style="display:inline;"><a href="?p=contact_calendar">Announcements</a></h2>
 											</header>
-											<a href="?p=pbl_workshops" class="image image-full"><img src="images/pbl+workshop+photo+for+website.jpg" alt="" /></a>
-											<ul class="actions">
-												<li><a href="#" class="button button-icon fa fa-file">Continue Reading</a></li>
-											</ul>
+<p><iframe style="border: 0;" src="https://www.google.com/calendar/embed?src=3pgodnitgpga0dl9368v4r0gck%40group.calendar.google.com&amp;ctz=America/Los_Angeles" width="80%" height="600px" frameborder="0" scrolling="no"></iframe></p>
 										</article>
 								
 								</div>
 								
 							<!-- Sidebar -->
 								<div id="sidebar" class="4u">
-								<?
-								/*
 									<!-- Excerpts -->
 										<section>
 											<ul class="divided">
-												<li>
+                                            	<li>
 													<!-- Excerpt -->
 														<article class="is-excerpt">
 															<header>
-																<span class="date">February 05, 2014</span>
+																<span class="date">Upcoming Events</span>
 																<h3><a href="#">iPoly Information Session for Potential Students</a></h3>
 															</header>
 															<p>1:45-3:00pm</p>
-														</article>
-												</li>
-												<li>
-													<!-- Excerpt -->
-														<article class="is-excerpt">
+                                                            <header>
+																<h3><a href="#">iPoly Information Session for Potential Students</a></h3>
+															</header>
+															<p>1:45-3:00pm</p>
 															<header>
-																<span class="date">February 06, 2014</span>
 																<h3><a href="#">iPoly Information Session for Potential Students</a></h3>
 															</header>
 															<p>8:15-9:30am</p>
-														</article>
-												</li>
-                                                <li>
-													<!-- Excerpt -->
-														<article class="is-excerpt">
-															<header>
-																<span class="date">February 07, 2014</span>
-																<h3><a href="#">Pep Rally - Small Gym, Cal Poly</a></h3>
-															</header>
+                                                            	<h3><a href="#">Pep Rally - Small Gym, Cal Poly</a></h3>
 															<p>11:00am - 12:30pm</p>
-														</article>
-												</li>
-                                                <li>
-													<!-- Excerpt -->
-														<article class="is-excerpt">
-															<header>
-																<span class="date">February 08, 2014</span>
+                                                            <header>
 																<h3><a href="#">Winter Formal - Meridian Club, Fullerton</a></h3>
 															</header>
 															<p>6:00pm - 11:00pm</p>
 														</article>
-												</li>									
+												</li>								
                                              </ul>
 										</section>
-								
-								
-									<!-- Highlights -->
-										<section>
-											<ul class="divided">
-												<li>
-													<!-- Highlight -->
-														<article class="is-highlight">
-															
-														</article>												
-                                                </li>
-                                                
-											</ul>
-										</section>
-										
-								
-								?>
 								</div>
 
 						</div>
 					</div>
 
 			</div>
-			*/
-			?>
