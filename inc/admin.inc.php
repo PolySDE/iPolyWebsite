3c89170fe1088db25bc56f32984d78a4355a25d6
Carlos Cerrillos <carlos@cerrillostech.com>
2014-03-10 16:40:36 -0700

    boring Admin page

---
+++ inc/admin.inc.php
@@ -0,0 +1,48 @@
+		<!-- Main Wrapper -->
+			<div id="main-wrapper">
+
+				<!-- Main -->
+					<div id="main" class="container">
+						<div class="row">
+						
+							<!-- Sidebar -->
+								<div id="sidebar" class="4u">
+								
+									<!-- Excerpts -->
+										<section>
+											<ul class="divided">
+												<li>
+
+													<!-- Excerpt -->
+														<article class="is-excerpt">
+															<header>
+																<span class="date" style="margin-bottom:1em;">Admin Panel</span>
+																<h3 style="padding-bottom:0; margin-bottom:0;"><a href="#">9th English</a></h3>
+                                                                <h3 style="padding-bottom:0; margin-bottom:0;"><a href="#">9th Math</a></h3>
+															</header>
+														</article>
+
+												</li>
+											</ul>
+										</section>
+								</div>
+
+							<!-- Content -->
+								<div id="content" class="8u skel-cell-important">
+
+									<!-- Post -->
+										<article class="is-post">
+											<header>
+												<h2>Title</h2>
+											</header>
+                                            <?
+											//Content
+											?>
+										</article>
+								
+								</div>
+
+						</div>
+					</div>
+
+			</div>
