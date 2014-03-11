6092b3ed41a2a790854cfa26cef6c5572b206453
Carlos Cerrillos <carlos@cerrillostech.com>
2014-03-10 22:03:52 -0700

    Admin Panel Beautification

--- inc/admin.inc.php
+++ inc/admin.inc.php
@@ -4,7 +4,6 @@
 				<!-- Main -->
 					<div id="main" class="container">
 						<div class="row">
-						
 							<!-- Sidebar -->
 								<div id="sidebar" class="4u">
 								
@@ -12,13 +11,23 @@
 										<section>
 											<ul class="divided">
 												<li>
-
 													<!-- Excerpt -->
 														<article class="is-excerpt">
 															<header>
-																<span class="date" style="margin-bottom:1em;">Admin Panel</span>
-																<h3 style="padding-bottom:0; margin-bottom:0;"><a href="#">9th English</a></h3>
-                                                                <h3 style="padding-bottom:0; margin-bottom:0;"><a href="#">9th Math</a></h3>
+																<?
+                                                                if(isset($_SESSION['is_admin'])){
+                                                                ?>
+                                                              
+                                                                    <span class="date" style="margin-bottom:1em;">Admin Panel</span>
+                                                                    <h3 style="padding-bottom:0; margin-bottom:0;"><a href="?p=admin&ap=acc">Account Settings</a></h3>
+                                                                    <h3 style="padding-bottom:0; margin-bottom:0;"><a href="?p=admin&ap=perms">Permissions</a></h3>
+                                                                <?
+                                                                } else {
+                                                                ?>
+                                                                	<span class="date" style="margin-bottom:1em">Not Logged In!</span>
+                                                                <?
+																}
+																?>
 															</header>
 														</article>
 
@@ -26,23 +35,37 @@
 											</ul>
 										</section>
 								</div>
-
 							<!-- Content -->
 								<div id="content" class="8u skel-cell-important">
-
 									<!-- Post -->
+                                    <?
+									if(isset($_GET['ap'])){
+										$ap = $_GET['ap'];
+									} else {
+										$ap = "";
+									}
+									$found = false;
+									if($ap=="perms"){
+										$found = true;
+									?>
+                                        <article class="is-post">
+                                            <header>
+                                            	<h2>Permissions</h2>
+                                            </header>
+                                            <p>Your permissions:</p>
+                                        </article>
+                                    <?
+									} else if($ap=="acc" || $found == false){
+									?>
 										<article class="is-post">
 											<header>
-												<h2>Title</h2>
+												<h2>Account Settings</h2>
 											</header>
-                                            <?
-											//Content
-											?>
 										</article>
-								
+                                    <?
+									}
+									?>
 								</div>
-
 						</div>
 					</div>
-
 			</div>
