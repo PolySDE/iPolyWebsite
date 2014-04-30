c9c27ace0966c2d9f07c74c75b0fdae05443e4e7
Carlos Cerrillos <carlos@cerrillostech.com>
2014-04-29 17:26:48 -0700

    Robotics

---
+++ robotics/index.php
@@ -0,0 +1,367 @@
+<!DOCTYPE HTML>
+<!--
+	Tessellate 1.0 by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Tessellate by HTML5 UP</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link href="http://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,400,400italic" rel="stylesheet" type="text/css" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel-noscript.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-wide.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<section id="header" class="dark">
+				<header>
+					<h1>iPoly Robotics Team</h1>
+					<p>concordia res parvae crescent</p>
+				</header>
+				<footer>
+					<a href="#first" class="button scrolly">proceed and discover</a>
+				</footer>
+			</section>
+			
+		<!-- First -->
+			<section id="first" class="main">
+				<header>
+					<div class="container">
+						<h2>The iPoly Robotics Team is focused in the endless pursuit of three verbs:</h>
+					</div>
+				</header>
+				<div class="content dark style1 featured">
+					<div class="container">
+						<div class="row">
+							<div class="4u">
+								<section>
+									
+									<header>
+										<h3>to inspire . . .</h3>
+									</header>
+									<p>As a team, we participate in several outreach events to inspire the people we meet about the potential Science and Engineering has in changing the world. It becomes our job to influence our youth that Science and Engineering are rewarding fields to enter because it will place them at the frontlines of the solutions that challenge our world. And because we love what we do, it's easier than said.</p>
+								</section>
+							</div>
+							<div class="4u">
+								<section>
+									
+									<header>
+										<h3>to innovate . . .</h3>
+									</header>
+									<p>Being an active member of the team enables us to be as innovative as our immagination allows us to be. Sometimes it's hard to trust our potential, but when we believe in our learning capabilities from the support of our parents, mentors, and even peers, we find ourselves with great designs. As we find new ideas, we realize that the only thing that inhibits innovation is the negativity we have for ourselves. </p>
+								</section>
+							</div>
+							<div class="4u">
+								<section>
+									
+									<header>
+										<h3>to compete . . .</h3>
+									</header>
+									<p>We acknowledge that great ideas can come from anywhere. Competing at events with fellow peers allows us the opportunity to test out our designs and even learn from our competitors. At the competitions we have participated in, we also realize that it's more than just our designs that matter. Our success is based on our ability to be creative and work with other teams strategizing to a common victory.</p>
+								</section>
+							</div>
+						</div>
+						<div class="row">
+							<div class="12u">
+								<footer>
+									<a href="#second" class="button scrolly">find out what we are up to</a>
+								</footer>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		<!-- Second -->
+			<section id="second" class="main">
+				<header>
+					<div class="container">
+						<h2>stay informed with even the little changes</h2>
+						<p>we invite you to keep in touch with our day to day progress and be a part of our team.</p><br>
+                 		</br>
+						<ul class="icons">
+					<li><a href="#" class="fa fa-instagram solo"><span>Instagram</span></a></li>
+					<li><a href="#" class="fa fa-facebook solo"><span>Facebook</span></a></li>
+					<li><a href="#" class="fa fa-youtube solo"><span>Youtube</span></a></li>
+				</ul>
+				</header>
+				<div class="content dark style2">
+					<div class="container">
+						<div class="row">
+							<div class="container">
+								<section>
+									<h3>What's new for the iPoly Robotics Team</h3>
+									<p>In the next few weeks, the iPoly Robotics Team will host a conference to summarize the last few months of our accomplishments, and triumphs to a community audience. The purpose of this conference is to organize new members, mentors and consolidate resources for next year's team. Currently, the team has every aspiration to expand but a lack in resources has been a significant factor in halting progress. The future status of the team after the departure of graduating Oversight Executives will be announced and newly appointed officers will be officially introduced. As of late April, this conference is still being coordinated by Overight Executives. </p>
+									<p>Members of the team expect in attendace, engineering students from Cal-Poly Pomona, among other invited guests in STEM related careers. iPoly students and parents passionate in a future with STEM are highly encuraged to attend so that they may determine the future they may have with the iPoly Robotics Team. At this moment, a conference is expected to be held if not once then, twice each school year.</p>
+									<footer>
+										<a href="#third" class="button scrolly">learn more about us</a>
+									</footer>
+								</section>
+							</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			
+		<!-- Third -->
+			<section id="third" class="main">
+				<header>
+					<div class="container">
+                    	<h2>about the team</h2>
+                        <p>discover everything there is to know about the iPoly Robotics Team</p>
+				</header>
+				<div class="content dark style3">
+					<div class="container">
+						
+						
+								<h3>Who we are.</h3>
+								<p>We grew from a group of about a dozen students crammed inside of a Cal-Poly Pomona study room that orginally met during Fridays, and Sundays. The team was an activity separated from school, so we got used to being self-sufficient and we grew closer as a group. Apart from Cal-Poly mentors we created a system where the club would be managed by Oversight Executives (team members with executive responsibilities to the team). From there, the team was departmentalized between Programming and Mechanical Design Departments.
+                                <p>We understood that we didn't need the support of our school to follow our heart. So for a few months, we just went with the flow. Our rookie season brought us in front several opportunities which enabled our team to grow even further. Today as for every tomorrow, we hope to bring that same motivation, and success as we expand our team by participating in more events, outreach activities, and inspire new students to join our team.</p>
+                                <h3>From past to present . . .</h3>
+                               <p>The iPoly Robotics team is a collection of students with a burning passion to pursue careers in the field of STEM. The group was originally formed in September of 2013 by the collaborative efforts of Cal-Poly and iPoly students. The group started wthout the initial support of the iPoly Administration and it was the result of pure student perserverance which led the team to success. While under the guidance of Cal-Poly engineering mentors the team competed in FIRST (For Inspiration as Recognition of Science and Technology) events under the aliases Inland Empire Broncos, the League of iPoly, and more commonly FTC (FIRST Tech Challenge) Team 7528.<p>
+                               <p>After the team's rookie season concluded with an award-winning finish, the team achieved an official affiliation with International Polytechnic High School (iPoly). As a new member of the iPoly family the team has undergone a name change to incorporate the iPoly name. The team's strategy meetings were also relocated to iPoly for added convenience.<p>
+								<p>Currently, the team is managed by members of Oversight which comprise of student officers who have the following titles: CEO (Chief Executive Officer), COO (Chief of Operations Officer), CFO (Chief Financial Officer), CPO (Chief Procurement Officer), CSO (Chief of Science Officer) and chief secretary. Beyond Oversight, the team is departmentalized into Mechanical Design, Public Relations and Programming Departments with individual presidents. To the iPoly administration, the team is represented by advisors Dale Hunzeker and Tony Pang.<p>
+								<p>Created by students from the classes of 2014, 2015, and 2017, the iPoly Robotics team is designed to create an environment for members to present hands-on learning and problem solving skills throughout the greater Southern California region.<p>
+                                <p>Founding members include: Mahmood Baggia ('17), Sara Baggia ('15), Carlos Cerrillos ('14), Alejandro Espinoza ('14), Adrian Galvan ('17), Carl La Russa ('15), Maryela Mendoza ('17), Noah Nakielski ('14), Regina Ongowarsito ('14), Bryan Posada ('14), Miguel Samonte ('14), Brendan Trang ('14), and Marina Williams ('17).<p>
+								<footer>
+									<a href="#fourth" class="button scrolly">Send us a message</a>
+								</footer>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		<!-- Basic Elements -->
+		<!--
+			<section class="main">
+				<header>
+					<div class="container">
+						<h2>A Whole Lotta Elements</h2>
+						<p>General purpose elements for every general purpose. Or something like that.</p>
+					</div>
+				</header>
+				<div class="content style1 dark">
+					<div class="container">
+						<section>
+							<header>
+								<h3>Paragraph</h3>
+								<p>This is a byline</p>
+							</header>
+							<p>Phasellus nisl nisl, varius id <sup>porttitor sed pellentesque</sup> ac orci. Pellentesque 
+							habitant <strong>strong</strong> tristique <b>bold</b> et netus <i>italic</i> malesuada <em>emphasized</em> ac turpis egestas. Morbi 
+							leo suscipit ut. Praesent <sub>id turpis vitae</sub> turpis pretium ultricies. Vestibulum sit 
+							amet risus elit.</p>
+						</section>
+						<section>
+							<header>
+								<h3>Blockquote</h3>
+							</header>
+							<blockquote>Fringilla nisl. Donec accumsan interdum nisi, quis tincidunt felis sagittis eget.
+							tempus euismod. Vestibulum ante ipsum primis in faucibus. Cras sit amet urna eros, id egestas
+							tempus ante ipsum primis in faucibus orci luctus et ultrices.</blockquote>
+						</section>
+						<section>
+							<header>
+								<h3>Divider</h3>
+							</header>
+							<p>Donec consectetur <a href="#">vestibulum dolor et pulvinar</a>. Etiam vel felis enim, at viverra 
+							ligula. Ut porttitor sagittis lorem, quis eleifend nisi ornare vel. Praesent nec orci 
+							facilisis leo magna. Cras sit amet urna eros, id egestas urna. Quisque aliquam 
+							tempus euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices 
+							posuere cubilia.</p>
+							<hr />
+							<p>Donec consectetur vestibulum dolor et pulvinar. Etiam vel felis enim, at viverra 
+							ligula. Ut porttitor sagittis lorem, quis eleifend nisi ornare vel. Praesent nec orci 
+							facilisis leo magna. Cras sit amet urna eros, id egestas urna. Quisque aliquam 
+							tempus euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices 
+							posuere cubilia.</p>
+						</section>
+						<section>
+							<header>
+								<h3>Unordered List</h3>
+							</header>
+							<ul class="default">
+								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
+								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
+								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
+								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
+							</ul>
+						</section>
+						<section>
+							<header>
+								<h3>Ordered List</h3>
+							</header>
+							<ol class="default">
+								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
+								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
+								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
+								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
+							</ol>
+						</section>
+						<section>
+							<header>
+								<h3>Table</h3>
+							</header>
+							<div class="table-wrapper">
+								<table class="default">
+									<thead>
+										<tr>
+											<th>ID</th>
+											<th>Name</th>
+											<th>Description</th>
+											<th>Price</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td>00001</td>
+											<td>Lorem ipsum dolor</td>
+											<td>Ut porttitor sagittis lorem quis nisi ornare.</td>
+											<td>29.99</td>
+										</tr>
+										<tr>
+											<td>00002</td>
+											<td>Sit amet nullam</td>
+											<td>Ut porttitor sagittis lorem quis nisi ornare.</td>
+											<td>19.99</td>
+										</tr>
+										<tr>
+											<td>00003</td>
+											<td>Feugiat felis viverra</td>
+											<td>Ut porttitor sagittis lorem quis nisi ornare.</td>
+											<td>29.99</td>
+										</tr>
+										<tr>
+											<td>00004</td>
+											<td>Sagittis enim felis</td>
+											<td>Ut porttitor sagittis lorem quis nisi ornare.</td>
+											<td>19.99</td>
+										</tr>
+										<tr>
+											<td>00005</td>
+											<td>Nullam sed vestibulum</td>
+											<td>Ut porttitor sagittis lorem quis nisi ornare.</td>
+											<td>19.99</td>
+										</tr>
+									</tbody>
+									<tfoot>
+										<tr>
+											<td colspan="3"></td>
+											<td>100.00</td>
+										</tr>
+									</tfoot>
+								</table>
+							</div>
+						</section>
+						<section>
+							<header>
+								<h3>Form</h3>
+							</header>
+							<form method="post" action="#">
+								<div class="row half">
+									<div class="6u">
+										<input class="text" type="text" name="name" id="name" value="" placeholder="John Doe" />
+									</div>
+									<div class="6u">
+										<input class="text" type="text" name="email" id="email" value="" placeholder="johndoe@domain.tld" />
+									</div>
+								</div>
+								<div class="row half">
+									<div class="12u">
+										<div class="select">
+											<select name="department" id="department">
+												<option value="">Choose a department</option>
+												<option value="1">Manufacturing</option>
+												<option value="2">Administration</option>
+												<option value="3">Support</option>
+											</select>
+										</div>
+									</div>
+								</div>
+								<div class="row half">
+									<div class="12u">
+										<input class="text" type="text" name="subject" id="subject" value="" placeholder="Enter your subject" />
+									</div>
+								</div>
+								<div class="row half">
+									<div class="12u">
+										<textarea name="message" id="message" placeholder="Enter your message"></textarea>
+									</div>
+								</div>
+								<div class="row">
+									<div class="12u">
+										<ul class="actions">
+											<li><input type="submit" class="button" value="Send Message" /></li>
+											<li><input type="reset" class="button alt" value="Clear Form" /></li>
+										</ul>
+									</div>
+								</div>
+							</form>
+						</section>
+					</div>
+				</div>
+			</section>
+		-->
+		
+		<!-- Fourth -->
+			<section id="fourth" class="main">
+				<header>
+					<div class="container">
+						<h2>Inquiries</h2>
+						<p>If you would like to know when our next meeting is, have ideas or suggestions you would like to share with us, or if you have general questions even, send a message below and we promise to get back to you shortly.</p>
+					</div>
+				</header>
+				<div class="content style4 featured">
+					<div class="container small">
+						<form method="post" action="#">
+							<div class="row half">
+								<div class="6u"><input type="text" class="text" placeholder="Name" /></div>
+								<div class="6u"><input type="text" class="text" placeholder="Email" /></div>
+							</div>
+							<div class="row half">
+								<div class="12u"><textarea name="message" placeholder="Message"></textarea></div>
+							</div>
+							<div class="row">
+								<div class="12u">
+									<ul class="actions">
+										<li><input type="submit" class="button" value="Send Message" /></li>
+										<li><input type="reset" class="button alt" value="Clear Form" /></li>
+									</ul>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</section>
+			
+		<!-- Footer -->
+			<section id="footer">
+				<ul class="icons">
+					<li><a href="#" class="fa fa-instagram solo"><span>Instagram</span></a></li>
+					<li><a href="#" class="fa fa-facebook solo"><span>Facebook</span></a></li>
+					<li><a href="#" class="fa fa-youtube solo"><span>Youtube</span></a></li>
+				</ul>
+				<div class="copyright">
+					<ul class="menu">
+                    <div class="container">
+					</ul>
+				</div>
+			</section>
+	</body>
+</html>
