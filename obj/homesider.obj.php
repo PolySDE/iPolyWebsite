720d3368136e72cb4429e60398d51f784e5633ce
Carlos Cerrillos <carlos@cerrillostech.com>
2014-05-13 12:26:40 -0700

    Objectified static entries for home sider

--- obj/homesider.obj.php
+++ obj/homesider.obj.php
@@ -7,12 +7,21 @@ class PolyEvent {
 	}
 }
 class PolySider {
+	private $title;
 	private $events;
-	function __construct($events = array()){
+	function __construct($events = array(), $title = "This Week At IPOLY HS"){
+		$this->title = $title;
 		$this->events = $events;
+		$this->fetchData();
 	}
 	function addEvent($event){
 		array_push($this->events, $event);
+	}
+	function fetchData(){
+		$this->addEvent(new PolyEvent("AP Calculus Testing", "8:00am - 12:00pm"));
+		$this->addEvent(new PolyEvent("Grad Night Cut Off", "Grad Night Cut Off"));
+		$this->addEvent(new PolyEvent("Exit Interviews", "Seniors"));
+		$this->addEvent(new PolyEvent("Drama Production", "Drama Elective"));
 	}
 	function paint(){
 		?>
@@ -21,26 +30,17 @@ class PolySider {
                                         <ul class="divided">
                                             <li>
                                                 <article class="is-excerpt">
-                                                <span class="date" style="margin-bottom:10px;">This Week At IPOLY HS</span>
+                                                <span class="date" style="margin-bottom:10px;"><? echo $this->title; ?></span>
+                                                <?
+												for($x = 0; $x < count($this->events); $x++){
+													?>
                                                     <header style="margin-bottom:0px; margin-top:0px; padding-left:0px;">
-                                                        <h3 style="margin-bottom:0px;"><a href="#">AP Calculus Testing</a></h3>
+                                                        <h3 style="margin-bottom:0px;"><a href="#"><? echo $this->events[$x]->title; ?></a></h3>
                                                     </header>
-                                                    <p style="margin-bottom:10px; margin-top:0px;padding-left:30px;">8:00am - 12:00pm</p>
-                                                    
-                                                    <header style="margin-bottom:0px; margin-top:0px; padding-left:0px;">
-                                                        <h3 style="margin-bottom:0px;"><a href="#">Grad Night Cut Off</a></h3>
-                                                    </header>
-                                                    <p style="margin-bottom:10px; margin-top:0px;padding-left:30px;">Seniors</p>
-                                                    
-                                                    <header style="margin-bottom:0px; margin-top:0px; padding-left:0px;">
-                                                        <h3 style="margin-bottom:0px;"><a href="#">Exit Interviews</a></h3>
-                                                    </header>
-                                                    <p style="margin-bottom:10px; margin-top:0px;padding-left:30px;">Seniors</p>
-                                                    
-                                                    <header style="margin-bottom:0px; margin-top:0px; padding-left:0px;">
-                                                        <h3 style="margin-bottom:0px;"><a href="#">Drama Production</a></h3>
-                                                    </header>
-                                                    <p style="margin-bottom:10px; margin-top:0px;padding-left:30px;">Drama Elective</p>
+                                                    <p style="margin-bottom:10px; margin-top:0px;padding-left:30px;"><? echo $this->events[$x]->content; ?></p>
+                                                    <?
+												}
+												?>
                                                 </article>
                                             </li>								
                                          </ul>
