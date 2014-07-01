d9b06384c37f81c127e9f0707596e1157941f7dd
Carlos Cerrillos <carlos@cerrillostech.com>
2014-07-01 15:30:49 -0700

    Header Admin Page work in progress

--- inc/admin.inc.php
+++ inc/admin.inc.php
@@ -21,6 +21,8 @@
                                                                     <span class="date" style="margin-bottom:1em;">Admin Panel</span>
                                                                     <h3 style="padding-bottom:0; margin-bottom:0;"><a href="?p=admin&ap=acc">Account Settings</a></h3>
                                                                     <h3 style="padding-bottom:0; margin-bottom:0;"><a href="?p=admin&ap=perms">Permissions</a></h3>
+                                                                    
+                                                                    <h3 style="padding-bottom:0; margin-bottom:0;"><a href="?p=admin&ap=header">Header Links</a></h3>
                                                                 <?
                                                                 } else {
                                                                 ?>
@@ -55,6 +57,45 @@
                                             <p>Your permissions:</p>
                                         </article>
                                     <?
+									} else if($ap=="header"){
+										$fount = true;
+										?>
+                                        <article class="is-post">
+                                        	<header>
+                                            	<h2>Header Links</h2>
+                                            </header>
+                                        	<p>
+                                            <?
+											$dbh = new mysqli($db_host, $db_user, $db_pass, $db_name);
+											if($dbh->connect_errno >0){
+												die("Mysql ERROR!! " . $dbh->connect_error);
+											}
+											if($queryh = $dbh->prepare("SELECT * FROM `settings` WHERE `id`='headerlinks'")){
+												$queryh->execute();
+												$queryh->bind_result($obj['id'], $obj['value']);
+												while($queryh->fetch()){
+													$object = unserialize(base64_decode($obj['value']));
+												}
+												$queryh->close();
+											}
+											$dbh->close();
+											$headerChildren = $object->getChildren();
+											
+											?>
+                                            <table>
+                                                <tr>
+                                                	<td>Parent</td>
+                                                    <td>Child</td>
+                                                </tr>
+                                                <?
+												for($x = 0; $x < count($headerChildren); $x++){
+													
+												}
+												?>
+                                            </table>
+                                            </p>
+                                        </article>
+                                        <?
 									} else if($ap=="acc" || $found == false){
 									?>
 										<article class="is-post">
