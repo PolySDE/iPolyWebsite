40089d423c1c3ee9f64f7fd246896bed7f482157
Carlos Cerrillos <carlos@cerrillostech.com>
2014-07-02 21:37:57 -0700

    Added Alphabet array for header editing functions

--- func/config.func.php
+++ func/config.func.php
@@ -5,4 +5,5 @@ $db_user = "quiz";
 $db_pass = "quiz";
 $db_name = "iPoly";
 date_default_timezone_set("America/Los_Angeles");
+$alphabet = array("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
 ?>
